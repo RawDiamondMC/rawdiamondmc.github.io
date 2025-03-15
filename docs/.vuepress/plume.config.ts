6de699419a9527b8624d22ec7d807f85f426d1a2
RawDiamondMC <RawDiamondMC@outlook.com>
2025-03-15 23:02:51 +0800

    Nice

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -6,7 +6,7 @@ import { notes } from './notes'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://zh.minecraft.wiki/images/Diamond_JE2_BE2.png?36a4d&format=original',
+  logo: 'https://s2.loli.net/2025/03/15/8EzegPl2yqTdSNa.png',
   appearance: true,
   social: [
     { icon: 'github', link: 'https://github.com/RawDiamondMC/' },
@@ -19,7 +19,7 @@ export default defineThemeConfig({
     message: 'Powered by <a target="_blank" href="https://wolf.snowlyicewolf.club/">AmarokIce</a> & <a target="_blank" href="https://github.com/HO-Artisan/">HO-Artisan</a><br/><a href="https://icp.gov.moe/?keyword=20259182" target="_blank">萌ICP备20259182号</a>'
   },
   profile: {
-    avatar: 'https://zh.minecraft.wiki/images/Diamond_JE2_BE2.png?36a4d&format=original',
+    avatar: 'https://s2.loli.net/2025/03/15/8EzegPl2yqTdSNa.png',
     name: 'RawDiamondMC 的个人博客',
     description: '',
     circle: true,
